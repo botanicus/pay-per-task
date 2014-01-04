0b211b73da2ee14d5f5b5d3613644e54c3c5bb48
James C Russell <james@101ideas.cz>
2014-01-04 17:48:26 +0100

    Deployment is pretty much done.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,10 +4,8 @@
 # It is NOT meant to run with Vagrant, neither it
 # is meant to run ON the remote server.
 
-~/Dropbox/Code/deployment/server/bin/deployer.sh ppt deployment/ruby.sh #deployment/git-deploy.sh
+~/Dropbox/Code/deployment/server/bin/deployer.sh ppt deployment/setup_rabbitmq.sh deployment/nginx.sh deployment/ruby.sh deployment/git-deploy.sh
 
-# git push --set-upstream origin master
-# git config alias.deploy 'push origin master:deployment'
-# git deploy
-
-# ~/Dropbox/Code/deployment/server/bin/deployer.sh ppt deployment/nginx.sh deployment/setup_rabbitmq.rb
+git push --set-upstream origin master
+git config alias.deploy 'push origin master:deployment'
+git deploy
--- deployment/nginx.sh
+++ deployment/nginx.sh
@@ -3,6 +3,3 @@
 cat > /etc/nginx/sites-enabled/$1.conf << EOF
 include /webs/$1/vhost.conf;
 EOF
-
-stop nginx 2> /dev/null
-start nginx
