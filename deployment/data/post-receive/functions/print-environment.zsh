f7cae532420d3d258b608a6747456fd4087ee2b7
James C Russell <james@101ideas.cz>
2014-01-04 18:36:50 +0100

    Testing deployment.

--- deployment/data/post-receive/functions/print-environment.zsh
+++ deployment/data/post-receive/functions/print-environment.zsh
@@ -3,8 +3,7 @@ debug "PATH: $PATH"
 debug "USER: $USER"
 debug "PWD: $PWD"
 debug "TERM: $TERM"
-debug "Ruby: $(which ruby) $(ruby -v)"
+debug "Ruby: $(which ruby)"
 debug
-debug "$NAME: $BRANCH"
-debug "$SOURCE -> $TARGET"
+debug "$NAME#$BRANCH: $SOURCE -> $TARGET"
 debug
