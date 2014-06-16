aa73fee849cfee21d339caf13be1edcda956ce01
James C Russell <james@101ideas.cz>
2014-06-16 17:52:23 +0200

    CDN.

--- deployment/provisioners/hosts.sh
+++ deployment/provisioners/hosts.sh
@@ -10,6 +10,7 @@ sudo tee -a /etc/hosts <<EOF
 127.0.0.1 pay-per-task.dev
 127.0.0.1 app.pay-per-task.dev
 127.0.0.1 blog.pay-per-task.dev
+127.0.0.1 cdn.pay-per-task.dev
 
 127.0.0.1 docs.pay-per-task.dev
 EOF
