dcf1da6f2af7f00d86657d064877829bd63d1a0f
James C Russell <james@101ideas.cz>
2014-06-18 06:38:43 +0000

    After running provisioners.

--- deployment/provisioners/dotfiles.sh
+++ deployment/provisioners/dotfiles.sh
@@ -7,10 +7,11 @@ cd
 git clone https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
 
 if test -d dotfiles; then
-  ./dotfiles/install.sh | sh
+  ./dotfiles/install.sh | grep -v ssh
+  ./dotfiles/install.sh | grep -v ssh | sh
 else
   tarball="https://github.com/botanicus/dotfiles/tarball/master"
   curl -L $tarball 2> /dev/null | tar -xzv --strip-components 1 --exclude={README.md,.editorconfig,.gitignore,install.sh,.ssh/config}
-  #curl https://raw.githubusercontent.com/botanicus/dotfiles/master/.ssh/config >> ~/.ssh/config
+  curl https://raw.githubusercontent.com/botanicus/dotfiles/master/.ssh/config >> ~/.ssh/config
 fi
 
