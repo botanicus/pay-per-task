a48895630f904dc3b4abd7967b7d63596f3fe191
James C Russell <james@101ideas.cz>
2014-06-20 21:28:34 +0200

    More powerful presenters.

--- consumers/lib/ppt/presenters.rb
+++ consumers/lib/ppt/presenters.rb
@@ -3,7 +3,13 @@ require 'ppt/extensions'
 class PPT
   module Presenters
     class Entity
-      EXPECTED_KEYS ||= []
+      def self.attributes
+        @attributes ||= Hash.new
+      end
+
+      def self.attribute(name, options = Hash.new)
+        self.attributes[name] = options
+      end
 
       attr_reader :values
 
@@ -11,9 +17,20 @@ class PPT
         # Let's consider it safe since this is not user input.
         # It might not be the best idea, but for now, who cares.
         values = PPT.symbolise_keys(values)
+        expected_keys = self.class.attributes.select { |name, options| options[:required] }.map { |name, _| name }.sort
 
-        unless values.keys.sort == self.class::EXPECTED_KEYS.sort
-          raise ArgumentError.new("Expected keys: #{self.class::EXPECTED_KEYS.inspect}, got #{values.keys.inspect}")
+        unless values.keys.sort == expected_keys
+          raise ArgumentError.new("Expected keys: #{expected_keys.inspect}, got #{values.keys.sort.inspect}")
+        end
+
+        private_keys = self.class.attributes.select { |name, options| options[:private] }.map { |name, _| name }
+        if union = values.keys & private_keys
+          raise ArgumentError.new("The following keys are private: #{union.inspect}")
+        end
+
+        # Set the defaults.
+        self.class.attributes.select { |name, options| options[:default] }.each do |name, block|
+          values[name] = self.instance_eval(&block)
         end
 
         @values = values
@@ -40,16 +57,31 @@ class PPT
     #
     # Each user can have only one service, just to make it simple.
     # Besides, not many people use both Jira and Pivotal Tracker.
+    require 'securerandom'
+
     class User < Entity
-      EXPECTED_KEYS = [:service, :username, :name, :email, :accounting_email]
+      attribute :service, required: true
+      attribute :username, required: true
+      attribute :name, required: true
+      attribute :email, required: true
+      attribute :accounting_email, default: Proc.new { self.email }
+      attribute :auth_key, private: true, default: Proc.new { Securerandom.hex }
     end
 
     class Developer < Entity
-      EXPECTED_KEYS = [:company, :username, :name, :email]
+      attribute :company, required: true
+      attribute :username, required: true
+      attribute :name, required: true
+      attribute :email, required: true
     end
 
     class Story < Entity
-      EXPECTED_KEYS = [:company, :id, :price, :currency, :link]
+      attribute :company, required: true
+      attribute :id, required: true
+      attribute :title, required: true
+      attribute :price, required: true
+      attribute :currency, required: true
+      attribute :link, required: true
     end
   end
 end
