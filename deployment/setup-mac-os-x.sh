0a3b97ed6e6245d989123d668b01c0442cf8c4f5
James C Russell <james@101ideas.cz>
2014-06-22 01:31:10 +0200

    Fixed the OS X setup script.

--- deployment/setup-mac-os-x.sh
+++ deployment/setup-mac-os-x.sh
@@ -1,20 +1,37 @@
 #!/bin/sh
 
 # The repo was cloned from GitHub, so GitHub is the origin.
-git remote rename origin github
+git remote rename origin github 2> /dev/null
 
 # Add server.
-git remote add server server:/repos/ppt
+git remote add server server:/repos/ppt 2> /dev/null
 
 # Set up Git remote tracking.
 git push --set-upstream github master
 git push --set-upstream server master
 
-# Set up alias to git deploy.
+# Set up an alias to git deploy.
 
-#git config alias.deploy 'push server'
-# TODO: This should support -f for forced deployments.
-git config alias.deploy "\!sh -c 'test -z \"\$1\" && git push server \$(git rev-parse --abbrev-ref HEAD) || git push server master' -"
+# Version 1:
+# git config alias.deploy 'push server'
+#
+# Version 2:
+# git config alias.deploy "\!sh -c 'test -z \"\$1\" && git push server \$(git rev-parse --abbrev-ref HEAD) || git push server master' -"
+#
+# Version 3:
+#
+# There has to be a better solution. Maybe something with read?
+git_deploy_script=$(cat <<EOF
+  force  = ARGV.delete("-f") || ARGV.delete("--force") || ""
+  branch = ARGV.shift || %x(git rev-parse --abbrev-ref HEAD).chomp
+
+  abort "Usage: git deploy [branch] [-f]" unless ARGV.empty?
+
+  puts "git push server #{branch} #{force}"
+  system "git push server #{branch} #{force}"
+EOF)
+
+git config alias.deploy "\!ruby -e '$git_deploy_script' --"
 
 # Make sure to push to master before deploying.
 tee .git/hooks/pre-push <<EOF
@@ -34,34 +51,42 @@ chmod +x .git/hooks/pre-push
 vagrant plugin install vagrant-triggers
 
 # Disable asking your password every time you run vagrant [up|halt]:
-sudo tee -a /etc/sudoers <<EOF
+if ! egrep '%wheel\s+ALL=\(ALL\)\s*NOPASSWD:\s*ALL' /etc/sudoers; then
+  sudo tee -a /etc/sudoers <<EOF
 # This enables all the users in the wheel group to run any command with sudo without password.
 # We're using it for port forwarding. It'd be best to allow only the pfctl command to be run
 # without password, but James couldn't figure out why nor did he consider it a priority.
 # In fact it's rather handy. Here we go then:
 %wheel ALL=(ALL) NOPASSWD: ALL
 EOF
+fi
 
-sudo tee -a /etc/hosts <<EOF
-# PPT Domains for Local Development.
-127.0.0.1 in.pay-per-task.dev
-127.0.0.1 api.pay-per-task.dev
+domains=(
+  in.pay-per-task.dev
+  api.pay-per-task.dev
 
-127.0.0.1 in.pay-per-task.test
-127.0.0.1 api.pay-per-task.test
+  in.pay-per-task.test
+  api.pay-per-task.test
 
-127.0.0.1 pay-per-task.dev
-127.0.0.1 app.pay-per-task.dev
-127.0.0.1 blog.pay-per-task.dev
-127.0.0.1 cdn.pay-per-task.dev
+  pay-per-task.dev
+  app.pay-per-task.dev
+  blog.pay-per-task.dev
+  cdn.pay-per-task.dev
+  docs.pay-per-task.dev
+)
 
-127.0.0.1 docs.pay-per-task.dev
-EOF
+for domain in $domains; do
+  if ! egrep $domain /etc/hosts; then
+    tee -a /etc/hosts "127.0.0.1 $domain"
+  fi
+done
 
-tee -a ~/.ssh/config <<EOF
+if ! egrep 'Host server' ~/.ssh/config; then
+  tee -a ~/.ssh/config <<EOF
 # PPT Linode server.
 Host server
   HostName 178.79.138.233
   User root
   compression yes
 EOF
+fi
