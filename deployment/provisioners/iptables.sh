9634050ab26433f3a86ce34f1706d83c5677b582
James C Russell <james@101ideas.cz>
2014-06-24 14:20:00 +0000

    After running provisioners.

--- deployment/provisioners/iptables.sh
+++ deployment/provisioners/iptables.sh
@@ -18,41 +18,28 @@
 #
 # Alright, ready?
 
-# Flush all rules in the default table (filter).
-
-
+# TODO:
 # Connection limit and rate limit. You want your firewall to be able to slow things down in the event of a DDoS attack or repeated attempts at port-knocking or things like that.
 # Block all unnecessary out-going traffic. This can help in case your machine is compromised as most bots need to connect to a remote command-and-control server.
 # Block all other incoming ports. You should really still block everything else, just in case an attacker manages to install some app and opens up a back-door port.
 
+# Flush all rules in the default table (filter).
 sudo iptables -F
+
 sudo iptables -P INPUT DROP
 sudo iptables -A INPUT -i lo -p all -j ACCEPT
 sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
 sudo iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
 sudo iptables -A INPUT -j DROP
 
-
-exit
-sudo iptables -F
-sudo iptables -X
-
-sudo iptables -t filter --list
-exit
-
-# Set default filter policy.
-sudo iptables -P INPUT DROP
-#sudo iptables -P OUTPUT DROP
-#sudo iptables -P FORWARD DROP
-# all ports except 80 port tcp, 53 udp(dns), 67,68 udp(dhcp).
-sudo iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
-sudo iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
-
-# Allow unlimited traffic on loopback
-sudo iptables -A INPUT -i lo -j ACCEPT
-sudo iptables -A OUTPUT -o lo -j ACCEPT
+sudo apt-get install -y iptables-persistent
 
 # NOTES:
+#
 # Port forwarding:
 # iptables -t nat -A PREROUTING -i $EXT_IF -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 8443
+#
+# List:
+# sudo iptables -t filter --list
