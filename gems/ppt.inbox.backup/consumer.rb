e2b2dda51177ce0533c27c52394cbea75ab37aa2
James C Russell <james@101ideas.cz>
2014-06-27 16:06:44 +0000

    It works.

--- gems/ppt.inbox.backup/consumer.rb
+++ gems/ppt.inbox.backup/consumer.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
 
-require 'bundler'
-
-Bundler.setup
+# By this we add everything into $LOAD_PATH,
+# but that doesn't matter, since we don't actually
+# require gems from any other group than default,
+# we only do it ad-hoc: require 'pry'; binding.pry
+require 'bundler/setup'
 
 require 'ppt'
 require 'json'
@@ -13,7 +15,9 @@ module PPT::PT
     QUEUES = {'inbox.backup' => 'inbox.#'}
 
     def run
-      client.consumer('inbox.backup', 'inbox.#') do |blob, header, frame|
+      client.consumer('inbox.backup', 'inbox.#') do |payload, header, frame|
+        puts "~ ppt.inbox.backup consumer is up and running."
+
         _, service, username = frame.routing_key.split('.')
         path = File.join('data', 'inbox', service, "#{username}.yml")
         FileUtils.mkdir_p(File.dirname(path))
@@ -21,7 +25,7 @@ module PPT::PT
 
         begin
           # Converting to JSON in order to leave out the whitespace.
-          minified_blob = JSON.parse(payload).to_json
+          minified_payload = JSON.parse(payload).to_json
 
           # By saving a lot of small files, 5k requests consumed 20MB
           # of disk space (5k * 4kB). With one big file we only need
@@ -37,7 +41,7 @@ module PPT::PT
           # including psych gem in our Gemfile for tests when we
           # read the YAML. Psych is however the default on MRI.
           File.open(path, 'a') do |file|
-            file.puts("---\n#{minified_blob}")
+            file.puts("---\n#{minified_payload}")
           end
         rescue Exception => error
           # No error should really happen, but we cannot
