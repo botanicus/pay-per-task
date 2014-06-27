54ab8aef3e6aa06ecf4786f9929960594e2a0d8d
James C Russell <james@101ideas.cz>
2014-06-27 14:42:45 +0000

    Rewritten to pipeline.rb.

--- gems/ppt.inbox.backup/consumer.rb
+++ gems/ppt.inbox.backup/consumer.rb
@@ -1,44 +1,50 @@
 #!/usr/bin/env ruby
 
+require 'bundler'
+
+Bundler.setup
+
 require 'ppt'
-require 'ppt/client'
-
 require 'json'
+require 'pipeline/plugin'
 
-# Save everything to the disk, so anything can be replayed.
-PPT.async_loop do |client|
-  client.on_open do
-    puts "~ Listening for data ..."
+module PPT::PT
+  class Consumer < Pipeline::Plugin
+    QUEUES = {'inbox.backup' => 'inbox.#'}
 
-    client.consumer('inbox.backup', 'inbox.#') do |payload, header, frame|
-      _, service, username = frame.routing_key.split('.')
-      path = File.join('data', 'inbox', service, "#{username}.yml")
-      FileUtils.mkdir_p(File.dirname(path))
-      puts "~ Writing payload from #{frame.routing_key} to #{path}"
+    def run
+      client.consumer('inbox.backup', 'inbox.#') do |blob, header, frame|
+        _, service, username = frame.routing_key.split('.')
+        path = File.join('data', 'inbox', service, "#{username}.yml")
+        FileUtils.mkdir_p(File.dirname(path))
+        puts "~ Writing payload from #{frame.routing_key} to #{path}"
 
-      begin
-        # Leave out whitespace.
-        blob = JSON.parse(payload).to_json
+        begin
+          # Leave out whitespace.
+          blob = JSON.parse(payload).to_json
 
-        # By saving a lot of small files, 5k requests consumed 20MB
-        # of disk space (5k * 4kB). With one big file we only need
-        # 4.2MB (when using the sample data from Pivotal Tracker).
-        #
-        # Insterestingly enough this actually IS valid YAML and we
-        # can use YAML.load_documents(io) on it. So far there was
-        # no need to escape the JSON as a string, it gets properly
-        # parsed as YAML.
-        #
-        # At least on MRI. YAML.load_documents on Rubinius raise
-        # an exception.
-        File.open(path, 'a') do |file|
-          file.puts("---\n#{blob}")
+          # By saving a lot of small files, 5k requests consumed 20MB
+          # of disk space (5k * 4kB). With one big file we only need
+          # 4.2MB (when using the sample data from Pivotal Tracker).
+          #
+          # Insterestingly enough this actually IS valid YAML and we
+          # can use YAML.load_documents(io) on it. So far there was
+          # no need to escape the JSON as a string, it gets properly
+          # parsed as YAML.
+          #
+          # At least on MRI. YAML.load_documents on Rubinius raise
+          # an exception.
+          File.open(path, 'a') do |file|
+            file.puts("---\n#{blob}")
+          end
+        rescue Exception => error
+          # No error should really happen, but we cannot
+          # pressume that the JSON is valid for instance.
+          puts "~ ERROR #{error.class} #{error.message}"
         end
-      rescue Exception => error
-        # No error should really happen, but we cannot
-        # pressume that the JSON is valid for instance.
-        puts "~ ERROR #{error.class} #{error.message}"
       end
     end
   end
 end
+
+PPT::PT::Consumer.run(PPT.root)
