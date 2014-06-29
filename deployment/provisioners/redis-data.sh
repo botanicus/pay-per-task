9ea18bbf39db32061dab0bef9e448247ad39658b
James C Russell <james@101ideas.cz>
2014-06-29 18:26:32 +0200

    Test user up to date with the current state of the models.

--- deployment/provisioners/redis-data.sh
+++ deployment/provisioners/redis-data.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-redis-cli hmset users:ppt auth_key Wb9CdGTqEr7msEcPBrHPinsxRxJdM
+redis-cli HMSET users.ppt auth_key Wb9CdGTqEr7msEcPBrHPinsxRxJdM \
+                          pt.api_key 78525a130a030829876309975267aa6a \
+                          email james@101ideas.cz \
+                          created_at 2014-06-29T15:13:37+02:00
