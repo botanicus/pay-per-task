3380807375a1b17a2160d201c995e6ab0b56ad13
James C Russell <james@101ideas.cz>
2014-06-30 01:47:44 +0200

    Redis does have auth.

--- deployment/provisioners/redis-data.sh
+++ deployment/provisioners/redis-data.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# TODO: production: require auth in Redis http://redis.io/commands/auth (aside of iptables)
 redis-cli HMSET users.ppt auth_key Wb9CdGTqEr7msEcPBrHPinsxRxJdM \
                           pt.api_key 78525a130a030829876309975267aa6a \
                           email james@101ideas.cz \
