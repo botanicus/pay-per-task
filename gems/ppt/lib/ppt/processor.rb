fd4922a8ff1f318a482140dae1d0612b28a990d2
James C Russell <james@101ideas.cz>
2014-07-10 11:17:17 +0200

    Parsing price & currency improved, currencies standardised to their ISO codes.

--- gems/ppt/lib/ppt/processor.rb
+++ gems/ppt/lib/ppt/processor.rb
@@ -1,18 +1,31 @@
+require 'ppt/models'
+
 class PPT
   class Processor
+    CURRENCY_MAP = {'$' => 'USD', '£' => 'GBP', '€' => 'EUR'}
+
+    # CURRENCY_MAP['$']   # => 'USD'
+    # CURRENCY_MAP['USD'] # => 'USD'
+    CURRENCY_MAP.default_proc = Proc.new do |hash, key|
+      hash.has_key?(key) ? hash[key] : key
+    end
+
     def initialize(client)
       @client = client
     end
 
     # $20 £20 €20
-    def parse_price(title)
-      match = title.match(/([£€$])(\d+)/)
-      return match[2].to_i, match[1]
+    # @returns title, currency, price
+    # TODO: What if given currency isn't supported?
+    def parse_title(title)
+      match = title.match(/^(([£€$]|(GBP|EUR|USD) )?(\d+)\s+)?(.+)$/)
+      return match[5], CURRENCY_MAP[match[2]], match[4].to_i
     end
 
-    def parse_title(title)
-      title.match(/[£€$]\d+ (.+)/)[1]
-    end
+    # parse_title "Test"
+    # parse_title "10 Test"
+    # parse_title "GBP 10 Test"
+    # parse_title "$10 Test"
 
     def emit(event, data)
       routing_key = "events.#{event}"
@@ -37,5 +50,3 @@ class PPT
     end
   end
 end
-
-require 'ppt/models'
