9e2f327aac8358df9ccb09b2d2e2423e4c998bad
James C Russell <james@101ideas.cz>
2015-01-02 13:44:15 +0100

    Otherwise Git fails.

--- packer/scripts/extra/etc-git.sh
+++ packer/scripts/extra/etc-git.sh
@@ -1,4 +1,12 @@
 cd /etc
 git init
+
+# For this repository only.
+# Original error was:
+# fatal: unable to auto-detect email address
+# (got 'root@packer-virtualbox-iso-1420196094.(none)')
+git config user.email "root@localhost"
+git config user.name "Root"
+
 git add .
 git commit -m "Blankslate."
