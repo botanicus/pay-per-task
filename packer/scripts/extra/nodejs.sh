25e931bd708a051282219c953aeb23eb03e2f159
James C Russell <james@101ideas.cz>
2015-01-02 13:43:47 +0100

    Update to the current NodeJS version.

--- packer/scripts/extra/nodejs.sh
+++ packer/scripts/extra/nodejs.sh
@@ -1,8 +1,8 @@
 # Alternative: https://github.com/creationix/nvm
-wget http://nodejs.org/dist/v0.10.28/node-v0.10.28.tar.gz
-tar xzf node-v0.10.28.tar.gz
-cd node-v0.10.28
-./configure --prefix=/opt/node-0.10.28 && make && make install
+wget http://nodejs.org/dist/v0.10.35/node-v0.10.35.tar.gz
+tar xzf node-v0.10.35.tar.gz
+cd node-v0.10.35
+./configure --prefix=/opt/node-0.10.35 && make && make install
 
 . /etc/profile.d/ruby.sh
-ruby -pi -e 'sub(/"$/, ":/opt/node-0.10.28/bin\"")' /etc/environment
+ruby -pi -e 'sub(/"$/, ":/opt/node-0.10.35/bin\"")' /etc/environment
