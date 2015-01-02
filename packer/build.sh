a6ea49e9304ec057f6e48e3273098c60aca63749
James C Russell <james@101ideas.cz>
2015-01-02 13:47:35 +0100

    The final version of the build script.

--- packer/build.sh
+++ packer/build.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-#export PACKER_LOG=1
+export PACKER_LOG=1
 rm packer_virtualbox-iso_virtualbox.box || true
 packer build -only virtualbox-iso packer.json
-vagrant box remove vagrant_machine || true
-vagrant box add vagrant_machine packer_virtualbox-iso_virtualbox.box
-end
+vagrant box remove ubuntu-14.10-amd64 || true
+vagrant box add ubuntu-14.10-amd64 packer_virtualbox-iso_virtualbox.box
