7fd36e493552d27f492988d1ec6ac4b40cdf5d62
James C Russell <james@101ideas.cz>
2015-01-02 20:05:32 +0000

    After running provisioners.

--- packer/build.sh
+++ packer/build.sh
@@ -6,5 +6,5 @@ export PACKER_LOG=1
 rm ubuntu-14.10-amd64.box || true
 packer build -only virtualbox-iso packer.json
 mv packer_virtualbox-iso_virtualbox.box ubuntu-14.10-amd64.box # TODO: Can I do this in packer.json?
-vagrant box remove ubuntu-14.10-amd64 || true
+vagrant box remove ubuntu-14.10-amd64 --force || true
 vagrant box add ubuntu-14.10-amd64 ubuntu-14.10-amd64.box
