f016795712d75ecd6daba39350a3d8fa5ecc0c04
James C Russell <james@101ideas.cz>
2015-01-02 13:52:32 +0100

    Rename the output box.

--- packer/build.sh
+++ packer/build.sh
@@ -3,7 +3,8 @@
 set -e
 
 export PACKER_LOG=1
-rm packer_virtualbox-iso_virtualbox.box || true
+rm ubuntu-14.10-amd64.box || true
 packer build -only virtualbox-iso packer.json
+mv packer_virtualbox-iso_virtualbox.box ubuntu-14.10-amd64.box # TODO: Can I do this in packer.json?
 vagrant box remove ubuntu-14.10-amd64 || true
-vagrant box add ubuntu-14.10-amd64 packer_virtualbox-iso_virtualbox.box
+vagrant box add ubuntu-14.10-amd64 ubuntu-14.10-amd64.box
