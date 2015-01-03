20b9ef4cec37c0ec08d0b3cc35c00f58a2c2b911
James C Russell <james@101ideas.cz>
2015-01-03 16:47:29 +0000

    After running provisioners.

--- packer/scripts/base/apt.sh
+++ packer/scripts/base/apt.sh
@@ -5,6 +5,6 @@ apt-get -y install zlib1g-dev
 # apt-get -y install libruby ruby-dev libedit-dev
 
 # Rubinius dependencies http://rubini.us/doc/en/getting-started/requirements.
-apt-get -y install bison ruby-dev rake libyaml-dev libssl-dev libreadline-dev libncurses5-dev llvm llvm-dev libeditline-dev
+apt-get -y install bison ruby-dev rake libyaml-dev libssl-dev libreadline-dev libncurses5-dev llvm llvm-dev libeditline-dev libedit-dev
 
 apt-get -y install dkms
