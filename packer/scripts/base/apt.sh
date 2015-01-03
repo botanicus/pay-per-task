3b8c245a3f02a69f573d7fe7009a5b88fa9ac32e
James C Russell <james@101ideas.cz>
2015-01-03 14:44:23 +0000

    After running provisioners.

--- packer/scripts/base/apt.sh
+++ packer/scripts/base/apt.sh
@@ -1,6 +1,10 @@
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install linux-headers-$(uname -r) build-essential
-apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
-apt-get -y install libruby ruby-dev # Otherwise Rubinius installation fails.
+apt-get -y install zlib1g-dev
+# apt-get -y install libruby ruby-dev libedit-dev
+
+# Rubinius dependencies http://rubini.us/doc/en/getting-started/requirements.
+apt-get -y install bison ruby-dev rake libyaml-dev libssl-dev libreadline-dev libncurses5-dev llvm llvm-dev libeditline-dev
+
 apt-get -y install dkms
