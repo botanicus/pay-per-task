e00776e4506f4e0f79abd8b8d52ec54590fbf2ba
James C Russell <james@101ideas.cz>
2015-06-04 22:27:44 +0100

    WIP

--- webs/pay-per-task.com/build.sh
+++ webs/pay-per-task.com/build.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+echo "~ Installing Bower packages."
 cd content && bower install > /dev/null && cd -
 
+echo "~ Building the Docker image."
 docker build -t pay-per-task.com .
+
+echo "~ Running pay-per-task.com."
 docker run -d -p 80:80 pay-per-task.com
 
+echo "~ Installing protractor."
 npm install protractor #> /dev/null
+
+echo "~ Getting the XYZ"
 webdriver-manager update
 
+echo "~ Starting the XYZ"
 webdriver-manager start &
 sleep 2.5
 
