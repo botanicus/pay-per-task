2fceb1b3a7b91dbeff16bda9ee23a1be00d0ad09
James C Russell <james@101ideas.cz>
2015-06-05 11:37:09 +0100

    What if we run it in a subshell?

--- webs/pay-per-task.com/build.sh
+++ webs/pay-per-task.com/build.sh
@@ -10,13 +10,13 @@ echo "~ Running pay-per-task.com."
 docker run -d -p 80:80 pay-per-task.com || exit 1
 
 echo "~ Installing protractor."
-npm install protractor -g #> /dev/null || exit 1
+npm install protractor -g > /dev/null || exit 1
 
 echo "~ Getting the XYZ"
 webdriver-manager update || exit 1
 
 echo "~ Starting the XYZ"
-# webdriver-manager start &
+(webdriver-manager start) &
 # sleep 2.5
 
 echo "~ Running the integration tests."
