6f4dc23c317cae3a19e5960d9321e48d116966bb
James C Russell <james@101ideas.cz>
2015-06-05 16:14:02 +0100

    echo -> puts

--- webs/pay-per-task.com/deploy.rb
+++ webs/pay-per-task.com/deploy.rb
@@ -20,11 +20,11 @@ if repos['values'].find { |repo| repo['name'] == REPO_NAME }
   system "git commit -a -m 'Build from #{Time.now.strftime("%Y/%m/%d %H:%M")}'"
   system "git push"
 else
-  puts "~ Repository #{REPO_NAME} doesn't exist yet, creating."
-  system "curl -X POST --user #{BITBUCKET_CREDENTIALS} #{BITBUCKET_API}/repositories/botanicus/#{REPO_NAME}"
+  puts "~ Repository #{REPO_NAME} doesn't exist yet, creating a PUBLIC one."
+  system "curl -X POST --user #{BITBUCKET_CREDENTIALS} #{BITBUCKET_API}/repositories/botanicus/#{REPO_NAME} -d {\"scm\": \"git\"}"
   # As of now, it's impossible to create automated builds through the API as far as I know.
   # system "curl -X PUT --user #{DOCKERHUB_CREDENTIALS} #{DOCKERHUB_API}/v1/repositories/paypertask/#{REPO_NAME}/ -v -d '[]'"
-  echo ">>> CREATE automated build. So far this isn't supported in the API."
+  puts ">>> CREATE automated build. So far this isn't supported in the API."
   # connect them via webhooks
   # commit & push
   # tutum?
