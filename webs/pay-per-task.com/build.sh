2131a5fef0c9c54c64fcf962c73109aa98414882
James C Russell <james@101ideas.cz>
2015-06-05 12:09:51 +0100

    STFU

--- webs/pay-per-task.com/build.sh
+++ webs/pay-per-task.com/build.sh
@@ -4,10 +4,10 @@ echo "~ Installing Bower packages."
 cd content && bower install > /dev/null || exit 1 && cd -
 
 echo "~ Building the Docker image."
-docker build -t pay-per-task.com . || exit 1
+docker build -t pay-per-task.com . > /dev/null || exit 1
 
 echo "~ Running pay-per-task.com."
-docker run -d -p 80:80 pay-per-task.com || exit 1
+docker run -d -p 80:80 pay-per-task.com > /dev/null || exit 1
 
 echo "~ Installing protractor."
 npm install protractor -g > /dev/null || exit 1
