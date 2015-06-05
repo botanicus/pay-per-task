22b30f5be4572a0a3cc7c5b9eb7dd27a7f6a19d6
James C Russell <james@101ideas.cz>
2015-06-05 12:13:54 +0100

    Shhh!

--- webs/pay-per-task.com/build.sh
+++ webs/pay-per-task.com/build.sh
@@ -6,7 +6,7 @@ cd content && bower install > /dev/null || exit 1 && cd -
 echo "~ Building the Docker image."
 docker build -t pay-per-task.com . > /dev/null || exit 1
 
-echo "~ Running pay-per-task.com."
+echo "~ Running the pay-per-task.com image."
 docker run -d -p 80:80 pay-per-task.com > /dev/null || exit 1
 
 echo "~ Installing protractor."
@@ -16,7 +16,7 @@ echo "~ Getting the XYZ"
 webdriver-manager update > /dev/null || exit 1
 
 echo "~ Starting the XYZ"
-(webdriver-manager start > /dev/null || exit 1) &
+(webdriver-manager start &> /dev/null || exit 1) &
 sleep 2.5
 
 echo -e "\n~ Running the integration tests."
