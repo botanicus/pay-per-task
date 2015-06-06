46f1e82b0b5183a0f3f876d4487d033ce1c53fea
James C Russell <james@101ideas.cz>
2015-06-06 13:52:13 +0100

    Deployment.

--- bin/deploy.rb
+++ bin/deploy.rb
@@ -44,7 +44,7 @@ if repos['values'].find { |repo| repo['name'] == REPO_NAME }
 else
   puts "~ Repository #{REPO_NAME} doesn't exist yet, creating."
   json = {scm: 'git', is_private: true}.to_json
-  run "curl -X POST --user #{BITBUCKET_CREDENTIALS} -H 'Content-Type: application/json' #{BITBUCKET_API}/repositories/botanicus/#{REPO_NAME} -d '#{json}'"
+  run "curl -X POST --user #{BITBUCKET_CREDENTIALS} -H 'Content-Type: application/json' #{BITBUCKET_API}/repositories/botanicus/#{REPO_NAME}.git -d '#{json}'"
   # As of now, it's impossible to create automated builds through the API as far as I know.
   # run "curl -X PUT --user #{DOCKERHUB_CREDENTIALS} #{DOCKERHUB_API}/v1/repositories/paypertask/#{REPO_NAME}/ -v -d '[]'"
   # TODO: This should go into Slack, no one reads this.
@@ -55,6 +55,6 @@ else
   run "rm -rf #{ENV['ROOT']}/.git"
   run "git init"
   run "git add ."
-  run "git remote add origin bitbucket/botanicus/#{REPO_NAME}"
+  run "git remote add origin bitbucket/botanicus/#{REPO_NAME}.git"
   run "git push -u origin master"
 end
