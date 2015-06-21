7b256938bd6583887a2415dbbacfc90f72d92183
James C Russell <james@101ideas.cz>
2015-06-21 08:39:56 +0200

    Deployment.

--- bin/deploy.rb
+++ bin/deploy.rb
@@ -35,6 +35,9 @@ end
 # Add to instructions for Slack:
 # Go to your Bitbucket repo settings, find the Hooks section, and add https://hooks.slack.com/services/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 as a POST URL.
 repos = JSON.parse(%x{curl --user #{BITBUCKET_CREDENTIALS} #{BITBUCKET_API}/repositories/botanicus})
+build_dir = ['dist', 'content', '.'].select { |dir| Dir.exist?(dir) }
+Dir.chdir(build_dir)
+
 if repos['values'].find { |repo| repo['name'] == REPO_NAME }
   puts "~ Repository #{REPO_NAME} exists, updating."
   # clone it, replace the code, commit, push
@@ -57,8 +60,21 @@ else
   run "git commit -a -m 'Initial import from #{Time.now.strftime("%Y/%m/%d %H:%M")}'"
   run "git push -u origin master"
   # run "curl -X PUT --user #{DOCKERHUB_CREDENTIALS} #{DOCKERHUB_API}/v1/repositories/paypertask/#{REPO_NAME}/ -v -d '[]'"
-  run %{curl -X POST https://hooks.slack.com/services/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 -d '{"text": "@botanicus:\n1. Please create <https://registry.hub.docker.com/builds/bitbucket/botanicus/#{REPO_NAME}/|#{REPO_NAME} automated build>. Dockerhub does not have API for this. Also add https://registry.hub.docker.com/hooks/bitbucket to <https://bitbucket.org/botanicus/#{REPO_NAME}/admin/hooks|#{REPO_NAME} hooks> (type POST).\n2. Add <https://registry.hub.docker.com/u/paypertask/#{REPO_NAME}/settings/webhooks/add|Dockerhub webhooks> \n3. Then deploy it <https://dashboard.tutum.co/container/launch/#tab-image-privates|from private> or <https://dashboard.tutum.co/container/launch/#tab-image-community|from public build>. Do not forget to\n  - Expose and map ports and make them published if necessary.\n  - Turn autorestart on.\n  - Maybe some links and volumes?", "username": "Deployment notifications", "channel": "#general"}'}
+  text = DATA.read.gsub('#{REPO_NAME}', REPO_NAME)
+  run %{curl -X POST https://hooks.slack.com/services/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 -d '{"text": text, "username": "Deployment notifications", "channel": "#general"}'}
   # https://dashboard.tutum.co/container/service/show/30bc8721-55c3-4216-8172-b6386ae95627/#container-triggers -> generate
   # https://registry.hub.docker.com/u/paypertask/pay-per-task.com/settings/webhooks/ -> add the generated URL
   # tutum?
 end
+
+__END__
+@botanicus:
+
+1. Please create <https://registry.hub.docker.com/builds/bitbucket/botanicus/#{REPO_NAME}/|#{REPO_NAME} automated build>. Dockerhub does not have API for this.
+2. <https://registry.hub.docker.com/u/paypertask/#{REPO_NAME}/settings/deploykeys/|Add a deployment key>.
+3. Also add `https://registry.hub.docker.com/hooks/bitbucket` to <https://bitbucket.org/botanicus/#{REPO_NAME}/admin/hooks|#{REPO_NAME} hooks> (type `POST`).
+4. Add <https://registry.hub.docker.com/u/paypertask/#{REPO_NAME}/settings/webhooks/add|Dockerhub webhooks>
+5. Then deploy it <https://dashboard.tutum.co/container/launch/#tab-image-privates|from private> or <https://dashboard.tutum.co/container/launch/#tab-image-community|from public build>. Do not forget to:
+  - Expose and map ports and make them published if necessary.
+  - Turn autorestart on.
+  - Maybe some links and volumes?
